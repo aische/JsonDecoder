3899284f94a430250c7e5ab6af77c93ac3058459
daniel <dvde@gmx.net>
2017-03-12 11:28:58 +0100

    fix BT example json

--- src/examples/BT/BT.ts
+++ src/examples/BT/BT.ts
@@ -7,7 +7,7 @@ export type BT<a> = {
 
 
 // can be decoded as BT<number>
-export const json_bt1 : any = { value:1, left: { value:13, right: { value:1, left: { value:13 }, right: { value:12, right: { value:1, left: { value:13, right: { value:1, left: { xxvalue:13 }, right: { value:12 }} }, right: { value:12 }} }} }, right: { value:12 }};
+export const json_bt1 : any = { value:1, left: { value:13, right: { value:1, left: { value:13 }, right: { value:12, right: { value:1, left: { value:13, right: { value:1, left: { value:13 }, right: { value:12 }} }, right: { value:12 }} }} }, right: { value:12 }};
 
-export const json_bt1_invalid : any = { value:1, left: { value:13, right: { value:1, left: { xxxxx_value:13 }, right: { value:12, right: { value:1, left: { value:13, right: { value:1, left: { xxvalue:13 }, right: { value:12 }} }, right: { value:12 }} }} }, right: { value:12 }};
+export const json_bt1_invalid : any = { value:1, left: { value:13, right: { value:1, left: { xxxxx_value:13 }, right: { value:12, right: { value:1, left: { value:13, right: { value:1, left: { value:13 }, right: { value:12 }} }, right: { value:12 }} }} }, right: { value:12 }};
 
